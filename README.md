cb10b482223cd5cb8648ca0c5749957774294181
60rzvvbj <1808078515@qq.com>
2021-05-08 13:54:24 +0800

    add util

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Assembly language exercises
 
+##### [关于汇编环境的配置和工具包的说明](/util/README.md)
+
 ### 本项目仅作学习参考之用
---
+++ util/debugasm.py
@@ -0,0 +1,18 @@
+import os
+import sys
+s = sys.argv[1]
+s = s.replace('.asm', '')
+print('Start compiling ' + s + '.asm')
+print()
+
+os.system('msdos -v5.0 masm ' + s + '.asm;')
+os.system('msdos -v5.0 link ' + s + '.obj; ')
+os.system('del ' + s + '.obj')
+
+print('End of compilation')
+print()
+print('Start debugging:')
+os.system('msdos -v5.0 debug ' + s + '.exe')
+os.system('del ' + s + '.exe')
+print()
+print('End of debugging')
